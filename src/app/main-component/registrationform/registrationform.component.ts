7e7d7cee5170d205ca18659215b95748121b0938
Aditya Alwish Lakra <aditya@thelattice.in>
2021-12-18 05:27:18 +0530

    modification

--- src/app/main-component/registrationform/registrationform.component.ts
+++ src/app/main-component/registrationform/registrationform.component.ts
@@ -1,5 +1,10 @@
 import { Component, OnInit } from '@angular/core';
-import { FormGroup, FormControl, Validators } from '@angular/forms';
+import {
+  FormGroup,
+  FormControl,
+  Validators,
+  FormBuilder,
+} from '@angular/forms';
 
 @Component({
   selector: 'app-registrationform',
@@ -7,33 +12,60 @@ import { FormGroup, FormControl, Validators } from '@angular/forms';
   styleUrls: ['./registrationform.component.css'],
 })
 export class RegistrationformComponent implements OnInit {
-  RegistrationForm = new FormGroup({
-    first_name: new FormControl('', [
-      Validators.required,
-      Validators.pattern(/^[A-Za-z][A-Za-z]{2,29}$/),
-    ]),
-    last_name: new FormControl('', [
-      Validators.required,
-      Validators.pattern(/^[A-Za-z][A-Za-z]{2,29}$/),
-    ]),
-    mobile: new FormControl('', [
-      Validators.required,
-      Validators.pattern('^((\\+91-?)|0)?[0-9]{10}$'),
-    ]),
-    email: new FormControl('', [Validators.required, Validators.email]),
-    password: new FormControl('', [
-      Validators.required,
-      Validators.pattern(
-        /^(?=.*\d)(?=.*[!@#$%^&*])(?=.*[a-z])(?=.*[A-Z]).{5,8}$/
-      ),
-    ]),
-    confirm_password: new FormControl('', [Validators.required]),
-    birth_day: new FormControl('', [Validators.required]),
-    gender: new FormControl('', [Validators.required]),
-  });
+  RegistrationForm: FormGroup;
+  maxdate: Date;
 
-  constructor() {}
+  constructor(private formbuilder: FormBuilder) {
+    this.maxdate = new Date();
+    this.RegistrationForm = this.formbuilder.group(
+      {
+        first_name: new FormControl('', [
+          Validators.required,
+          Validators.pattern(/^[A-Za-z][A-Za-z]{2,29}$/),
+        ]),
+        last_name: new FormControl('', [
+          Validators.required,
+          Validators.pattern(/^[A-Za-z][A-Za-z]{2,29}$/),
+        ]),
+        mobile: new FormControl('', [
+          Validators.required,
+          // @TODO REGEX FOR PHONE NUMBER
+          Validators.pattern('^((\\+91-?)|0)?[0-9]{10}$'),
+        ]),
+        email: new FormControl('', [
+          Validators.required,
+          Validators.pattern(/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/),
+        ]),
+        password: new FormControl('', [
+          Validators.required,
+          Validators.pattern(
+            /^(?=.*?[A-Z])(?=.*?[a-z])(?=.*?[0-9])(?=.*?[#?!@$%^&*-]).{8,}$/
+          ),
+        ]),
+        confirm_password: new FormControl('', [Validators.required]),
+        birth_day: new FormControl('', [Validators.required]),
+        gender: new FormControl('', [Validators.required]),
+      },
+      {
+        validators: this.mustmatch('password', 'confirm_password'),
+      }
+    );
+  }
 
+  mustmatch(controlname: string, matchingcontrolname: string) {
+    return (formGroup: FormGroup) => {
+      const control = formGroup.controls[controlname];
+      const matchingcontrol = formGroup.controls[matchingcontrolname];
+      if (matchingcontrol.errors && !matchingcontrol.errors.mustmatch) {
+        return;
+      }
+      if (control.value !== matchingcontrol.value) {
+        matchingcontrol.setErrors({ mustmatch: true });
+      } else {
+        matchingcontrol.setErrors(null);
+      }
+    };
+  }
   ngOnInit(): void {}
 
   onSubmit() {
